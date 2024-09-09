dfadf7271066ae66c0f7be8a49574cbe4607f347
sdutill <shawndutill@gmail.com>
2024-09-09 10:41:02 -0400

    Formatted all python files

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import os
 import sys
 import time
-import yaml
 from typing import Literal
 
-from zabbix_objects.template import Template
+import yaml
 from utils.mib_validator import MIBValidator
+from zabbix_objects.template import Template
 
-def create_all_yaml(template: Template, include_items: bool = True, include_traps: bool = True, include_discovery_rules: bool = True) -> Literal['YAMLStr']:
+
+def create_all_yaml(
+    template: Template,
+    include_items: bool = True,
+    include_traps: bool = True,
+    include_discovery_rules: bool = True,
+) -> Literal["YAMLStr"]:
     """
     Create a YAML representation of the template and its components.
 
@@ -23,19 +29,29 @@ def create_all_yaml(template: Template, include_items: bool = True, include_trap
     template_yaml = template.generate_yaml_dict()
 
     if include_items and template.snmp_items:
-        snmp_item_yaml = [ snmp_item.generate_yaml_dict() for snmp_item in template.snmp_items]
-        template_yaml['zabbix_export']['templates'][0]['items'].extend(snmp_item_yaml)
-    
+        snmp_item_yaml = [
+            snmp_item.generate_yaml_dict() for snmp_item in template.snmp_items
+        ]
+        template_yaml["zabbix_export"]["templates"][0]["items"].extend(snmp_item_yaml)
+
     if include_traps and template.snmp_traps:
-        snmp_trap_yaml = [ snmp_trap.generate_yaml_dict() for snmp_trap in template.snmp_traps]
-        template_yaml['zabbix_export']['templates'][0]['items'].extend(snmp_trap_yaml)
-    
+        snmp_trap_yaml = [
+            snmp_trap.generate_yaml_dict() for snmp_trap in template.snmp_traps
+        ]
+        template_yaml["zabbix_export"]["templates"][0]["items"].extend(snmp_trap_yaml)
+
     if include_discovery_rules and template.discovery_rules:
-        discovery_rule_yaml = [ discovery_rule.generate_yaml_dict() for discovery_rule in template.discovery_rules]
+        discovery_rule_yaml = [
+            discovery_rule.generate_yaml_dict()
+            for discovery_rule in template.discovery_rules
+        ]
         if discovery_rule_yaml:
-            template_yaml['zabbix_export']['templates'][0]['discovery_rules'] = discovery_rule_yaml
+            template_yaml["zabbix_export"]["templates"][0][
+                "discovery_rules"
+            ] = discovery_rule_yaml
 
     return yaml.dump(template_yaml, default_flow_style=False, sort_keys=False)
+
 
 def main() -> None:
     """
@@ -53,35 +69,46 @@ def main() -> None:
         sys.exit(1)
 
     excel_file = sys.argv[1]
-    
+
     if not os.path.exists(excel_file):
         print(f"Error: File '{excel_file}' not found.")
         sys.exit(1)
 
     print("Extracting data from Excel...")
-    snmp_items_json_list, snmp_traps_json_list, template_info_json, discovery_rule_tables = MIBValidator.extract_from_excel(excel_file)
+    (
+        snmp_items_json_list,
+        snmp_traps_json_list,
+        template_info_json,
+        discovery_rule_tables,
+    ) = MIBValidator.extract_from_excel(excel_file)
 
     print("Creating Template...")
-    template = Template(template_info_json, snmp_items_json_list, snmp_traps_json_list, discovery_rule_tables)
+    template = Template(
+        template_info_json,
+        snmp_items_json_list,
+        snmp_traps_json_list,
+        discovery_rule_tables,
+    )
 
     print("Creating YAML...")
     yaml_template = create_all_yaml(template)
 
     print("Writing YAML to file...")
-    timestamp = time.strftime('%Y%m%d_%H%M%S')
-    output_dir = './created_templates'
-    output_file = f'{output_dir}/{timestamp} {template.name} Template.yaml'
+    timestamp = time.strftime("%Y%m%d_%H%M%S")
+    output_dir = "./created_templates"
+    output_file = f"{output_dir}/{timestamp} {template.name} Template.yaml"
 
     # Check if the directory exists, if not, create it
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
         print(f"Created directory: {output_dir}")
 
-    with open(output_file, 'w') as f:
+    with open(output_file, "w") as f:
         f.write(yaml_template)
 
     print(f"YAML template saved as '{output_file}'")
     print("Process completed successfully!")
+
 
 if __name__ == "__main__":
     main()
--- zabbix_objects/tag.py
+++ zabbix_objects/tag.py
@@ -1,4 +1,5 @@
-from typing import List, Dict
+from typing import Dict, List
+
 
 class Tag:
     def __init__(self, key: str, value: str):
@@ -7,14 +8,20 @@ class Tag:
 
     @staticmethod
     def _parse_tag_string(tag_string: str) -> List[List[str]]:
-        return [pair.strip().split(':') for pair in tag_string.strip('{}').split(',') if ':' in pair]
+        return [
+            pair.strip().split(":")
+            for pair in tag_string.strip("{}").split(",")
+            if ":" in pair
+        ]
 
     @classmethod
-    def generate_template_tags(cls, raw_tags: str, manufacturer: str, device: str) -> List['Tag']:
+    def generate_template_tags(
+        cls, raw_tags: str, manufacturer: str, device: str
+    ) -> List["Tag"]:
         tags = []
         tags.extend(cls._parse_tag_string(f"{{target:{manufacturer}}}"))
         tags.extend(cls._parse_tag_string(f"{{target:{device}}}"))
-        
+
         if raw_tags:
             tags.extend(cls._parse_tag_string(raw_tags))
 
@@ -24,16 +31,13 @@ class Tag:
     def _process_tag_key(key: str) -> str:
         key = str(key).strip()
         return key.lower().replace(" ", "_")
-    
+
     @staticmethod
     def _process_tag_value(value: str) -> str:
         value = str(value).strip()
         return value.lower().replace(" ", "_")
 
     def generate_yaml_dict(self) -> Dict[str, str]:
-        tag_yaml ={
-            'tag': self.key,
-            'value': self.value
-        }
-        
+        tag_yaml = {"tag": self.key, "value": self.value}
+
         return tag_yaml
